31bd2e992d4285d134d32b6396ae51ec0ae831e1
Sibham Maity <163507491+Sibham@users.noreply.github.com>
2026-09-15 23:10:38 +0530

    Implement rectangle overlap check function

---
+++ DAY 14/Rectangle Overlap.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    bool isRectangleOverlap(vector<int>& A, vector<int>& B) {
+        if((A[2]<=B[0] || A[0]>=B[2] || A[3]<=B[1] || A[1]>=B[3])) return false;
+        return true;
+    }
+};
